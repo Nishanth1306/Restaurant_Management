157557e648575a639b60916d02d6f15f950822f4
Nishanth <nishanthkumar1363@gmail.com>
2025-06-05 16:56:33 +0530

    Invoice controller added

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -39,19 +40,42 @@ func GetInvoice() gin.HandlerFunc {
 		invoiceId := c.Param("invoice_id")
 		var invoice models.Invoice
 
-		err := invoiceCollection.FindOne(ctx, bson.M{"invoice_id":invoiceId}).Decode(&invoice)
+		err := invoiceCollection.FindOne(ctx, bson.M{"invoice_id": invoiceId}).Decode(&invoice)
 		defer cancel()
 
-		if err != nil{
+		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error Occured While fetching the Invoice"})
-			return	
+			return
 		}
 		c.JSON(http.StatusOK, invoice)
 	}
 }
 func CreateInvoice() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		var invoice models.Invoice
+		if err := c.BindJSON(&invoice); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		invoice.Invoice_id = primitive.NewObjectID().Hex()
+		invoice.Created_at = time.Now()
+		invoice.Updated_at = time.Now()
+		invoice.ID = primitive.NewObjectID()
+		validationErr := validate.Struct(invoice)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+		result, insertErr := invoiceCollection.InsertOne(ctx, invoice)
+		if insertErr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while inserting the invoice"})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusOK, gin.H{"message": "Invoice created successfully", "invoice_id": result.InsertedID})
+		log.Println("Invoice created successfully:", result.InsertedID)
 	}
 }
 
